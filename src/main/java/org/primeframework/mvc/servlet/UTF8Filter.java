4543f8072edbcf9938b349c3aa76aefa49425d8d
James Humphrey <leafknode@gmail.com>
2012-11-07 10:41:50 -0700

    added UTF8 filter

---
+++ src/main/java/org/primeframework/mvc/servlet/UTF8Filter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2012, Inversoft Inc., All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.primeframework.mvc.servlet;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * This filter does nothing more than set the character encoding of the request to always be UTF-8.
+ *
+ * @author Brian Pontarelli
+ */
+public class UTF8Filter implements Filter {
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {
+  }
+}
