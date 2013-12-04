ed065a73caccd4001d975f29a2265f9ab2685615
Brian Pontarelli <brian@inversoft.com>
2013-12-04 15:56:44 -0700

    Added URL converter

---
+++ src/main/java/org/primeframework/mvc/parameter/convert/converters/URLConverter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2013, Inversoft Inc., All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.primeframework.mvc.parameter.convert.converters;
+
+import org.apache.commons.lang3.StringUtils;
+import org.primeframework.mvc.parameter.convert.AbstractGlobalConverter;
+import org.primeframework.mvc.parameter.convert.ConversionException;
+import org.primeframework.mvc.parameter.convert.ConverterStateException;
+import org.primeframework.mvc.parameter.convert.annotation.GlobalConverter;
+
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * This converts to and from URL.
+ *
+ * @author Seth Musselman
+ */
+@GlobalConverter
+public class URLConverter extends AbstractGlobalConverter {
+  protected String objectToString(Object value, Type convertFrom, Map<String, String> attributes, String expression)
+      throws ConversionException, ConverterStateException {
+    URL url = (URL) value;
+    return url.toString();
+  }
+
+  protected Object stringToObject(String value, Type convertTo, Map<String, String> attributes, String expression)
+      throws ConversionException, ConverterStateException {
+    if (StringUtils.isBlank(value)) {
+      return null;
+    }
+    try {
+      return new URL(value);
+    } catch (MalformedURLException e) {
+      throw new ConversionException(e);
+    }
+  }
+
+  protected Object stringsToObject(String[] values, Type convertTo, Map<String, String> attributes, String expression)
+      throws ConversionException, ConverterStateException {
+    throw new UnsupportedOperationException("You are attempting to map a form field that contains " +
+        "multiple parameters to a property on the action class that is of type java.net.URL. This isn't " +
+        "allowed.");
+  }
+}
